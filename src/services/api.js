0647450b102e0eaa49084076a83fd1702d8fe873
GAYOUNG914 <rkdud91455@gmail.com>
2025-10-20 17:40:06 +0900

    Switch to allorigins.win CORS proxy

--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,7 @@ const ALPHA_VANTAGE_API_KEY = import.meta.env.VITE_ALPHA_VANTAGE_API_KEY || 'QDQ
 
 // FRED API 기본 설정 (CORS 프록시 사용)
 const fredApi = axios.create({
-  baseURL: 'https://cors-anywhere.herokuapp.com/https://api.stlouisfed.org/fred',
+  baseURL: 'https://api.allorigins.win/raw?url=https://api.stlouisfed.org/fred',
   params: {
     api_key: FRED_API_KEY,
     file_type: 'json'
@@ -15,7 +15,7 @@ const fredApi = axios.create({
 
 // Alpha Vantage API 기본 설정 (CORS 프록시 사용)
 const alphaVantageApi = axios.create({
-  baseURL: 'https://cors-anywhere.herokuapp.com/https://www.alphavantage.co/query'
+  baseURL: 'https://api.allorigins.win/raw?url=https://www.alphavantage.co/query'
 })
 
 // 연준 대차대조표 데이터 가져오기
